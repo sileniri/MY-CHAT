d4f19cc1249fdcce6f4a59acef604d4b5f152ce7
sileniri <sigurrinnan@gmail.com>
2025-07-02 18:06:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ function changeTheme() {
 if (!user && !(location.href.includes("login") || location.href.includes("signup"))) {
     console.log("firstTime?", firstTime, "allowed", !user && !location.pathname.includes("signup"));
     if (firstTime) {
-        location.pathname = "client/signup";
+        location.pathname = "/signup";
     } else if (!firstTime) {
-        location.pathname = "client/login";
+        location.pathname = "/login";
     }
 } else if (user && (location.href.includes("login") || location.href.includes("signup"))) {
     location.href = "..";
@@ -38,7 +38,7 @@ function logout() {
     location.reload();
 }
 
-if (location.pathname === "/client/") {
+if (location.pathname === "/") {
     document.querySelector("#logout").addEventListener("click", () => {
         logout();
     });
@@ -195,7 +195,7 @@ if (location.href.includes("signup")) {
 
 if (location.pathname.includes("settings")) {
     document.querySelector("#exit-settings").addEventListener("click", () => {
-        location.href = "/client";
+        location.href = "/";
     });
 }
 console.log("line 185");
